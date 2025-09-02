24e605dcd70e58e5d35078857c02330e1b8f0772
“thechers” <matiass.fm13@gmail.com>
2025-09-02 10:00:15 -0300

    SisPersonal Casi Completo

--- SisPersonal/Empleados.cs
+++ SisPersonal/Empleados.cs
@@ -1,6 +1,6 @@
 namespace Empresa.Empleados;
 
-enum Cargos 
+public enum Cargos 
 {
    Auxiliar,
    Administrativo,
@@ -12,19 +12,38 @@ enum Cargos
 
 public class Empleado
 {
-    private string Nombre;
-    private string Apellido;
+    private string nombre;
+    private string apellido;
 
-    private DateTime FechaNac;
+    private DateTime fechaNac;
 
-    private char EstCiv;
+    private char estCiv;
 
-    private DateTime FechIng;
+    private DateTime fechIng;
 
-    private double SueldoBase;
+    private double sueldoBase;
 
-    private Cargos Cargo;
+    private Cargos cargo;
 
+    public string Nombre { get => nombre; set => nombre = value; }
+    public string Apellido { get => apellido; set => apellido = value; }
+    public DateTime FechaNac { get => fechaNac; set => fechaNac = value; }
+    public char EstCiv { get => estCiv; set => estCiv = value; }
+    public DateTime FechIng { get => fechIng; set => fechIng = value; }
+    public double SueldoBase { get => sueldoBase; set => sueldoBase = value; }
+    public Cargos Cargo { get => cargo; set => cargo = value; }
+
+
+    public Empleado(string nombre, string apellido, DateTime fechaNac, char estCiv, DateTime fechIng, double sueldoBase, Cargos cargo)
+    {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaNac = fechaNac;
+        this.estCiv = estCiv;
+        this.fechIng = fechIng;
+        this.sueldoBase = sueldoBase;
+        this.cargo = cargo;
+    }
 
     public int antiguedad()
     {
@@ -48,14 +67,14 @@ public class Empleado
 
     public int jubilacion()
     {
-        int anioNac = FechaNac.Year;
+        int anioNac = fechaNac.Year;
         int anioActual = DateTime.Now.Year;
         int edad = anioActual - anioNac;
 
         if (edad < 65)
         {
             int estimado = anioNac + 65;
-            int faltante = anioActual - estimado;
+            int faltante = estimado - anioActual;
 
             return faltante;
         }
@@ -64,6 +83,44 @@ public class Empleado
         {
             return 0;
         }
+
+    }
+
+    public double salario()
+    {
+        int anioActual = DateTime.Now.Year;
+        int anioIngreso = FechIng.Year;
+        int antiguedad = anioActual - anioIngreso;
+        double adicional, salario;
+
+        if (antiguedad <= 20)
+        {
+            adicional = (sueldoBase * (antiguedad / 100));
+        }
+        else
+        {
+            adicional = (sueldoBase * 0.25);
+        }
+
+        if (cargo == Cargos.Ingeniero || cargo == Cargos.Especialista)
+        {
+            adicional += adicional * 0.5;
+        }
+
+        if (EstCiv == 'C')
+        {
+            adicional += 150000;
+        }
+
+        salario = sueldoBase + adicional;
+
+        return salario;
+    }
+
+    public void MostrarDatos()
+    {
+
+        Console.WriteLine($"{Nombre} {Apellido} | Nac: {FechaNac.ToShortDateString()} | " + $"Estado Civil: {EstCiv} | Ingreso: {FechIng.ToShortDateString()} | " + $"Cargo: {Cargo}");
 
     }
 
--- SisPersonal/Program.cs
+++ SisPersonal/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using Empresa.Empleados;
+
+Empleado[] empleados = new Empleado[3];
+
+
+    empleados[0] = new Empleado("Juan","Perez",new DateTime(1994,02,13),'S',new DateTime(2020,10,12),500000,Cargos.Investigador);
+    empleados[1] = new Empleado("Ana", "Gómez", new DateTime(1985, 11, 3), 'C',new DateTime(2010, 6, 15), 250000, Cargos.Administrativo);
+    empleados[2] = new Empleado("Carlos", "Ruiz", new DateTime(1978, 1, 25), 'C',new DateTime(2005, 9, 20), 400000, Cargos.Ingeniero);
+
+double montoFinal = 0;
+foreach (Empleado emp in empleados)
+{
+    montoFinal += emp.salario();
+    emp.MostrarDatos();
+    
+}
+
+Console.WriteLine($"Monto Total: {montoFinal}");
